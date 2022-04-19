f4a4cc6a30d86351c33d0e5b73e60a5d9a3bc3d0
Ehiksalex <ehiksalex@gmail.com>
2022-04-19 23:57:32 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+function lifeInWeeks(age) {
+    
+/************Don't change the code above************/    
+        
+//Write your code here.
+    var days = (90-age) * 365;
+    var weeks = (90-age) * 52;
+    var months = (90-age) * 12;
+    
+    console.log("you have " + days + " days, " + weeks +" weeks, " + months + " months left");
+        
+        
+        
+        
+/*************Don't change the code below**********/
+}
+lifeInWeeks(89);   
